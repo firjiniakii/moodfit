9f073d7a72e6e2effc2c7991cc886bc880c8d2fb
firjiniakii <firjiniakii@gmail.com>
2026-06-14 12:51:33 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+  import { initializeApp } 
+  from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCSkpX51ZsFWXFE06F869UfnjxZkAAfY4A",
+    authDomain: "moodfit-77c7a.firebaseapp.com",
+    projectId: "moodfit-77c7a",
+    storageBucket: "moodfit-77c7a.firebasestorage.app",
+    messagingSenderId: "278596532420",
+    appId: "1:278596532420:web:84945a59e0a84cbc95b191",
+  
+  };
+  const app = initializeApp(firebaseConfig);
+
+export default app;
