b9057fd0d5a3db6cbfa2d7707afaa8f28017f598
firjiniakii <firjiniakii@gmail.com>
2026-06-14 09:05:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,3 +9,17 @@ document.addEventListener('click',e=>{
   a.style.display=a.style.display==='block'?'none':'block';
  }
 });
+
+const hamburger =
+document.querySelector('.hamburger');
+
+const navLinks =
+document.querySelector('.nav-links');
+
+hamburger.addEventListener('click',()=>{
+
+    hamburger.classList.toggle('active');
+
+    navLinks.classList.toggle('active');
+
+});
